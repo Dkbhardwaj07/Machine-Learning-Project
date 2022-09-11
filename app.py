4a27cd59c39b4361a3974bc3e41c6fcb62fa84d6
durgesh183 <durgeshbhardwaj5100@gmail.com>
2022-09-11 10:43:10 +0530

    updated

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from  flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
 app=Flask(__name__)
 
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting Machine Learning Project"
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        raise HousingException(e,sys) from e
+        logging.info(housing.error_message)
+        logging.info("we are testing logging module")
+    return "CI CD pipeline has been established"
 
 
 
